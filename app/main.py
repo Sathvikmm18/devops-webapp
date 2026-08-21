1149c3c2922078d5e8da4fc71f68cd7738898df5
Sathvikmm18 <sathvikmm18@gmail.com>
2026-08-21 14:41:13 +0530

    Fix EC2 SSH access

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ app = FastAPI(
 @app.get("/")
 def home():
     return {
-        "message": "Hello from Automated DevOps! Created by Sathvik M M",
+        "message": "Hello from Automated DevOps! Created by Sathvik M M DevSecOps Engineer",
         "status": "running"
     }
 
